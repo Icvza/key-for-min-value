4a3ddd2584f47ab066ee99c8e3aa47ef3b3d1db3
Isaac Chavez <devisaacalejandro@gmail.com>
2020-11-21 23:20:00 -0500

    commit tree 087c9dcbef810e3ced5ae2d117b8d1dc88cfaa41

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,10 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
-end
+    return nil if name_hash.length == 0
+    x, y = name_hash.first
+    name_hash.each do |key, value|
+       if value < y
+       x = key 
+       end
+    end
+    x
+ end
